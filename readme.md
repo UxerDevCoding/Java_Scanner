f0753a1019e2a5215599cb563ab51edd2546ead8
UxerDevCoding <omarheviaarbana@gmail.com>
2022-03-05 20:39:47 +0100

    Scanner exercise work correctly

---
+++ readme.md
@@ -0,0 +1,18 @@
+### Scanner
+
+## Exercise
+Write a program who ask two questions
+
+- What is your name?
+- How old are you?
+
+## Instructions
+The user respond in terminal Output:
+ - Welcome <name>, you have <age> you can pass.
+ - Welcome <name>, you have <age> you cannot pass
+
+## Stack
+Java
+
+## Author
+Omar Hevia
--- src/main/java/com/omar/App.java
+++ src/main/java/com/omar/App.java
@@ -1,17 +1,24 @@
 package com.omar;
 
-/**
- * Hello world!
- */
-public final class App {
-    private App() {
-    }
+import java.util.Scanner;
 
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
+public final class App {
+   
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Scanner init = new Scanner(System.in);
+
+        System.out.println("What is yout name?");
+        String userName= init.nextLine();
+        System.out.println("How old are you?");
+        int userAge=init.nextInt();
+
+        if (userAge >= 18) {
+            System.out.println("Welcome " + userName + ", you have " + userAge + " years, you can pass");
+        } else {
+            System.out.println("Welcome " + userName + ", you have " + userAge + " years, you cannot pass");
+        }
+        
+        init.close();
     }
 }
